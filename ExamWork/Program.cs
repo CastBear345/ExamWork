26ebab91004d688803d0f230e182be3871cd7021
Janarbek S <castbear42@gmail.com>
2023-12-02 15:19:58 +0600

    #1 Change

---
+++ ExamWork/Program.cs
@@ -0,0 +1,19 @@
+﻿//Здесь был MrNagibator3000(Janarbek)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartLight smartLight = new SmartLight();
+            SmartThermostat smartThermostat = new SmartThermostat();
+        }
+    }
+}
---
+++ ExamWork/SmartDevice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class SmartDevice
+{
+    public string DeviceName { get; set; }
+    public bool IsOn { get; set; }
+
+    public virtual void TurnOn() { }
+    public virtual void TurnOff() { }
+}
+
+// Класс для лампочек
+public class SmartLight : SmartDevice
+{
+    public int Brightness { get; set; }
+
+    public void AdjustBrightness(int level)
+    {
+        Brightness = level;
+    }
+
+    public override void TurnOn()
+    {
+        IsOn = true;
+    }
+
+    public override void TurnOff()
+    {
+        IsOn = false;
+    }
+}
+
+// Класс для термостата
+public class SmartThermostat : SmartDevice
+{
+    public int Temperature { get; private set; }
+
+    public void SetTemperature(int temperature)
+    {
+        Temperature = temperature;
+    }
+
+    public override void TurnOn()
+    {
+        IsOn = true;
+    }
+
+    public override void TurnOff()
+    {
+        IsOn = false;
+    }
+}
